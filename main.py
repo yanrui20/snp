c44ce71d041cf6512b4cfeff3e6e779149879a51
yanrui <yanr23@mails.tsinghua.edu.cn>
2024-11-01 12:20:16 +0800

    support DB filetype

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 import platform
 
 class SNP:
-    def __init__(self, filepath):
+    def __init__(self, filepath, filetype, n):
         self.filepath = filepath
-        self.filename, self.n = self.filename_process()
+        self.filetype = filetype.upper()
+        self.n = n
+        self.filename = self.filename_process()
         self.data = self.get_data()
     
     def filename_process(self):
@@ -16,13 +18,39 @@ class SNP:
             filename = filename.split("\\")[-1]
         else:
             filename = filename.split("/")[-1]
-        n = filename.split(".")[-1][1]
-        if not n.isdigit():
-            print("filename error")
-            exit(-1)
-        return filename, int(n)
+        return filename
 
     def get_data(self):
+        if self.filetype == "DB":
+            return self.process_DB_file()
+        elif self.filetype == "MA":
+            return self.process_MA_file()
+        else:
+            raise ValueError("Unknown File Type")
+    
+    def process_DB_file(self):
+        with open(self.filepath, "r") as f:
+            lines = f.readlines()
+        
+        header = lines[5][1:] + lines[6][1:] + lines[7][1:]
+        header = header.split()
+
+        lines = lines[8:]
+        pre_data = [(lines[i*3] + lines[i*3+1] + lines[i*3+2]).split() for i in range(len(lines // self.n))]
+        pre_data = pd.DataFrame(data=pre_data, columns=header).astype(float)
+
+        ## change to MA
+        data = pd.DataFrame()
+        data["freq[MHz]"] = pre_data["Freq"] * 1e3  ## GHz -> MHz
+        pairs = [f"S{i}{j}" for i in range(1, self.n+1) for j in range(1, self.n+1)]
+        
+        for pair in pairs:
+            data[f"re:{pair}"] = pre_data[f"DB{pair}"] * np.cos(pre_data[f"Ang{pair}"])
+            data[f"im:{pair}"] = pre_data[f"DB{pair}"] * np.sin(pre_data[f"Ang{pair}"])
+
+        return self.expand_MA_data(data)
+
+    def process_MA_file(self):
         with open(self.filepath, "r") as f:
             lines = f.readlines()
 
@@ -33,7 +61,11 @@ class SNP:
 
         data = pd.DataFrame(data=pre_data, columns=header).astype(float)
         data["freq[MHz]"] = data["freq[Hz]"] / 1e6
+        data.drop(columns=["freq[Hz]"])
 
+        return self.expand_MA_data(data)
+    
+    def expand_MA_data(self, data):
         pairs = [f"S{i}{j}" for i in range(1, self.n+1) for j in range(1, self.n+1)]
         for pair in pairs:
             data[f"{pair}[MODULUS]"] = np.abs(data[f"re:{pair}"] + 1j * data[f"im:{pair}"])
@@ -72,8 +104,9 @@ class SNP:
 
 
 if __name__ == "__main__":
-    s2p = SNP("BSL1.s2p")
-    # s2p = SNP(r"D:\xxx\BSL1.s2p") ## on windows
+    s2p = SNP(filepath="BSL1.s2p", filetype="ma", n=2)
+    # s2p = SNP(filepath=r"D:\xxx\BSL1.s2p", filetype="db", n=2) ## on windows
     s2p.draw(name="smith", pairs=["S11"])
-    s2p.draw(name="smith", pairs=["S11", "S22"], limitMHZ=[800, 1000])
-    s2p.draw(name="db", pairs=["S11", "S12", "S22"], limitMHZ=[800, 1000])
+    # s2p.draw(name="vswr", pairs=["S11", "S22"], limitMHZ=[800, 1000])
+    # s2p.draw(name="modulus", pairs=["S11", "S22"], limitMHZ=[800, 1000])
+    # s2p.draw(name="db", pairs=["S11", "S12", "S22"], limitMHZ=[800, 1000])
